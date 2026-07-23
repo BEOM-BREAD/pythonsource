752342385ac57aa62cd0e8990c02ca07cde2fb9d
BEOM <qjawn345@gmail.com>
2026-07-23 18:11:11 +0900

    csv,json,try ~ except,

---
+++ ch1/mod1.py
@@ -0,0 +1,21 @@
+# #calc.py 모듈 사용
+# import calc
+
+# print(calc.add(7, 5))
+# print(calc.sub(7,5))
+
+# from calc import add, sub, Math
+# print(add(7, 5))
+# print(sub(7, 5))
+
+# math = Math()
+# print(math.solv(5))
+
+from ch1.calc import *
+from ch1.mod2 import *
+from ch1.mod3 import *
+math = Math()
+print(math.solv(5))
+print(add(7, 5))
+print(sub(7, 5))
+max()
